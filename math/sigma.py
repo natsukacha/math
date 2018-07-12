b32bd1222b95002bef1903b31b810920d4e5cdfd
natsukacha <s1611537@klis.tsukuba.ac.jp>
2018-07-13 02:16:56 +0900

    1st

---
+++ math/sigma.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 13 02:06:20 2018
+
+@author: natsu
+"""
+
+print("数字を入力してください\n")
+n = input()
+i=0
+n=int(n)+1
+sig=0
+for i in range(n):
+   sig+=i
+   
+print(sig)   
